daf549cafa29fd9ca9fd5e64688c80654155b3ed
unknown <Minskoleopc>
2023-09-18 17:13:09 -0700

    added

---
+++ day1.py
@@ -0,0 +1,103 @@
+
+print("hello")
+balance = 0
+print(balance)
+balance = 1000
+print(balance)
+
+# Arithmetic
+
+# Addition        +
+# Subtraction     -
+# Multiplication  *
+# Division        /
+# Modulus         %
+s = 10
+t = 5
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+q = 8
+r = 4
+
+print(q+r)
+print(q-r)
+print(q*r)
+print(q/r)
+print(q%r)
+
+# 100 pairs  ---- 500 lines DNY
+
+# function
+
+def Calculator(x,y):
+    print(x+y)
+    print(x -y)
+    print(x * y)
+    print(x/y)
+    print(x % y)
+
+Calculator(9,3)
+Calculator(8,4)
+
+
+#  function  without parameter and function without return type
+def addA():
+    print(8+8)
+addA()
+addA()
+addA()
+addA()
+
+# function with parameter and without return type
+def addB(x,y):
+    print(x+y)
+
+addB(22,3)
+addB(7,3)
+
+# function with parameter and with return type
+#  $100  - given  ($100 + $100 , $100 / 5 , $100 * 0.10)
+#  $100  - show
+def addC(x,y):
+    return x + y  # 5
+q1 = addC(2,3)
+print(q1)
+print(q1 + q1)
+print(q1 / 5)
+print(q1 * 0.10)
+
+# ----->  100
+# discount    10 %  ----->    payment --- 90
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
